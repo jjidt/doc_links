ff70d784f3aaf5465f68afb82b109406b85557ba
J.J. Idt <johnidt@gmail.com>
2014-09-02 23:02:55 -0700

    fix flash messages from user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash.now.notice = "user created successfully"
+      flash[:notice] = "user created successfully"
       redirect_to :root
     else
-      flash.now.alert = "incomplete information, please try again"
+      flash[:alert] = "incomplete information, please try again"
       redirect_to :back
     end
   end
