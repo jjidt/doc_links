38a6a3fd3709a193ce8ecbe1c34762d25ffb2704
J.J. Idt <johnidt@gmail.com>
2014-09-02 22:06:03 -0700

    add create method to sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_name(params[:name])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash.now.notice = "You have successfully logged in"
+      redirect_to :root
+    else
+      flash.now.alert = "Incorrect information, please try again"
+    end
+  end
+
+  def destroy
+
+  end
+
+end
