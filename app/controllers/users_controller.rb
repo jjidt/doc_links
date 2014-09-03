eeb99ebe6044309c3fe459564c25a691d0b1a305
J.J. Idt <johnidt@gmail.com>
2014-09-02 18:33:13 -0700

    create users table and columns

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash.now.notice = "user created successfully"
+      redirect_to :root
+    else
+      flash.now.alert = "incomplete information, please try again"
+      redirect_to :back
+    end
+  end
 end
--- db/migrate/20140903005844_create_users.rb
+++ db/migrate/20140903005844_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.integer :password_digest
       t.timestamps
     end
   end
