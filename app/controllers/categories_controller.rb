72a6bf2fc4ad7f13b602e79e0baf6d170c18a6f3
J.J. Idt <johnidt@gmail.com>
2014-09-05 10:39:54 -0700

    correct category create controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,7 +9,25 @@ class CategoriesController < ApplicationController
   end
 
   def create
+    @user = current_user
+    if current_user
+      @category = @user.categories.new(category_params)
+      if @category.save
+        redirect_to categories_path
+      else
+        flash[:alert] = "Incomplete information, please try again"
+        redirect_to :back
+      end
+    else
+      flash[:alert] = "You must be logged in to do that"
+      redirect_to login_path
+    end
   end
 
+private
+
+  def category_params
+    params.require(:category).permit(:name, :user_id)
+  end
 
 end
