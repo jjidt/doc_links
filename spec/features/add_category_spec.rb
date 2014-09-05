e5794e21600046f24afa535a849b7d31d364479b
J.J. Idt <johnidt@gmail.com>
2014-09-05 11:45:42 -0700

    add integration test for incomplete information on category form

--- spec/features/add_category_spec.rb
+++ spec/features/add_category_spec.rb
@@ -2,13 +2,23 @@ require 'rails_helper'
 require 'login_helper'
 
 describe 'adding a category' do
-  it 'adds the category on a users page' do
+  before do
     user = FactoryGirl.create(:user)
     login({:name => 'john', :password => 'test'})
+  end
+
+  it 'adds the category on a users page' do
     click_link 'add-category'
     fill_in :name, :with => 'one'
     click_button 'submit'
     expect(page).to have_content 'categories'
     expect(page).to have_content 'one'
   end
+
+  it 'add displays an error if category name is blank' do
+    click_link 'add-category'
+    click_button 'submit'
+    expect(page).to have_content 'Incomplete information'
+  end
+
 end
