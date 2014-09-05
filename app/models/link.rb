ab21a8cbe1094fe741dc87629d19a7d76e8c2445
J.J. Idt <johnidt@gmail.com>
2014-09-05 16:49:11 -0700

    validation for presence of name and url in link

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,5 @@
 class Link < ActiveRecord::Base
   belongs_to :category
+  validates_presence_of :name
+  validates_presence_of :url
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 describe Link do
   it { should belong_to :category }
+  it { should validate_presence_of :url }
+  it { should validate_presence_of :name }
 end
