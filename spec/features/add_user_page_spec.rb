74f11757ed0fe2f0ef61f5d9a76af83a9a9ca0d9
J.J. Idt <johnidt@gmail.com>
2014-09-04 22:21:19 -0700

    add integration test for successfully creating user

--- spec/features/add_user_page_spec.rb
+++ spec/features/add_user_page_spec.rb
@@ -7,3 +7,15 @@ describe 'viewing a user' do
     expect(page).to have_content 'Create a new Profile'
   end
 end
+
+describe 'creating a new user' do
+  it 'should create a user and return to the homepage' do
+    visit('/')
+    click_link 'create-profile'
+    fill_in 'name', :with => 'user'
+    fill_in :password, :with => 'test'
+    fill_in :confirm_password, :with => 'test'
+    click_button 'Create User'
+    expect(page).to have_content 'user created successfully'
+  end
+end
