7de194d58772b254cec4bc883049ba8033a75572
J.J. Idt <johnidt@gmail.com>
2014-09-05 09:57:50 -0700

    add integration test for incorrecto password on login

--- spec/features/user_login_page_spec.rb
+++ spec/features/user_login_page_spec.rb
@@ -1,13 +1,23 @@
 require 'rails_helper'
 
 describe 'Logging in a user' do
+  before { user = FactoryGirl.create(:user) }
+
   it 'successfully logs in a user' do
-    user = FactoryGirl.create(:user)
     visit '/'
     click_link 'login'
     fill_in :name, :with => "john"
     fill_in :password, :with => "test"
     click_button ''
     expect(page).to have_content 'Logged in'
+  end
+
+  it 'displays an error message if the password is incorrect' do
+    visit '/'
+    click_link 'login'
+    fill_in :name, :with => "john"
+    fill_in :password, :with => "nope"
+    click_button ''
+    expect(page).to have_content 'Incorrect information'
   end
 end
