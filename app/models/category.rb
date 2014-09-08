3488a340a68be0b6216d3ae9fe2ef713e5a0b06f
J.J. Idt <johnidt@gmail.com>
2014-09-07 20:42:28 -0700

    test for relationship between categories and snippets

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveRecord::Base
   belongs_to :user
   has_many :links
+  has_many :snippets
   validates_presence_of :name
   validates_presence_of :user_id
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe Category do
   it { should belong_to :user }
   it { should have_many :links }
+  it { should have_many :snippets }
   it { should validate_presence_of :name }
   it { should validate_presence_of :user_id }
 end
